f4609a19ffedc0b2f4cc8a439458b45bf0d69cd3
dan1723 <dan.gomes1723@gmail.com>
2023-12-19 15:28:26 -0300

    alteração

--- Exemplo2/Program.cs
+++ Exemplo2/Program.cs
@@ -1,6 +1,42 @@
 ﻿using Exemplo2.Models;
+using System.Globalization;
 
-Pessoa p1 = new Pessoa("Danilo", "Fernandes");
+
+string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+foreach(string linha in linhas){
+
+    Console.WriteLine(linha);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Pessoa p1 = new Pessoa("Danilo", "Fernandes");
 Pessoa p2 = new Pessoa("José", "Silva");
 
 Curso Ingles = new Curso();
@@ -9,11 +45,7 @@ Ingles.Alunos = new List<Pessoa>();
 
 Ingles.AdicionarAluno(p1);
 Ingles.AdicionarAluno(p2);
-Ingles.ListarAlunos();
-
-int num = 23; 
-
-bool teste = true;
+Ingles.ListarAlunos();*/
 
 
 
@@ -21,14 +53,48 @@ bool teste = true;
 
 
 
+    /*string dataString = "2023-13-16 09:54";
+    
+    bool sucesso = DateTime.tryParseExact(dataString, 
+                                          "yyyy-MM-dd HH:mm",
+                                          CultureInfo.InvariantCulture,
+                                          DateTimeStyles.None,
+                                          out DateTime data);
+    
+    
+    if (sucesso){
+        Console.WriteLine($"Conversão com sucesso! Data: {data}");
+    } else{
+        Console.WriteLine($"{dataString} não é uma data valida");
+    }*/
+    
+    
+    
+    
+    
 
-
-
-
-
-
-
-
+    
+/*decimal valorMon = 1582.40M;
+    
+    Console.WriteLine(valorMon.ToString("N2", CultureInfo.CreateSpecificCulture("pt-BR")));
+    
+    double porcen = .3421;
+    
+    Console.WriteLine(porcen.ToString("P"));
+    
+    int num = 123456;
+ Console.WriteLine(num.ToString("##-##-##"));*/
+    
+    
+    
+    
+    
+/*string n1 = "10";
+    string n2 = "20";
+    
+    string result = n1 + n2;
+    
+Console.WriteLine(result);*/
 
 
 /*Pessoa p1 = new Pessoa();
